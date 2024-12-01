4f850f27641f8544955ad1eb44184f99bf5ac30a
Gokul Prakash K <gokulprakashk1@gmail.com>
2024-12-01 12:38:33 +0100

    Testing app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ import pandas as pd
 st.title('Article Recommendation System')
 st.info('This app recommends articles based on user search')
 
-df = pd.read_csv('./Sample.csv')
-df.head()
+with st.expander('Data'):
+    df = pd.read_csv('./Sample.csv')
+    df.head()
