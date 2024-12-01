e7d9bef89dd9c88e5b4ef1e8fd28621b92b1f696
Gokul <150864191+GokulPrakashK98@users.noreply.github.com>
2024-12-01 12:57:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ st.title('Article Recommendation System')
 st.info('This app recommends articles based on user search')
 
 with st.expander('Data'):
+    st.write('**Raw Data**')
     df = pd.read_csv('https://raw.githubusercontent.com/GokulPrakashK98/DataScienceProject/refs/heads/test-branch/Sample.csv?token=GHSAT0AAAAAAC2HKVXITY5SHOGYV636SVCAZ2MJ3CQ')
     df
