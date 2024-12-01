656819dbc105cde006869eb49c73167df65afe9b
Gokul <150864191+GokulPrakashK98@users.noreply.github.com>
2024-12-01 12:53:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('Article Recommendation System')
 st.info('This app recommends articles based on user search')
 
 with st.expander('Data'):
-    df = pd.read_csv('./Sample.csv')
+    df = pd.read_csv('Sample.csv')
     df.head()
